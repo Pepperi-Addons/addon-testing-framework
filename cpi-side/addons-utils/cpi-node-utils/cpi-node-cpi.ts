98dbf8eb08cbbfc8632396498f9e262101a22960
guyPepperi <guy.k@pepperi.com>
2024-09-18 14:11:06 +0300

    move logic to service

--- cpi-side/addons-utils/cpi-node-utils/cpi-node-cpi.ts
+++ cpi-side/addons-utils/cpi-node-utils/cpi-node-cpi.ts
@@ -1,15 +1,14 @@
 import { Relation } from '@pepperi-addons/papi-sdk';
-import fs from 'fs';
-import util from 'util';
-import {ADDON_TABLE_NAME, AddonMetaData, CPI_NODE_ADDON_VERSION_CHANGE_TEST_DATA_OBJECT, CPI_NODE_TEST_TABLE, CPI_NODE_VALIDATE_AFTER_SYNC_RELATION_CALL, CPINodeAddonUUId, VALIDATE_AFTER_SYNC_CALL_FIELD_NAME} from './consts';
-const readdirAsync = util.promisify(fs.readdir);
+import {CPI_NODE_VALIDATE_AFTER_SYNC_RELATION_CALL, VALIDATE_AFTER_SYNC_CALL_FIELD_NAME} from './consts';
+import { CPINodeService } from './services/cpi-node-service';
 
 export async function load(configuration: any) {
 }
 
 export const router = Router();
 router.post('/test_upsert_relation', async (req, res) => {
-    const relation = await pepperi.addons.data.relations.upsert(req.body);
+    const cpiNodeService = new CPINodeService();
+    const relation = await cpiNodeService.upsertRelation(req.body);
     res.json({ relation });
 });
 
@@ -20,60 +19,40 @@ router.post('/test_after_sync_relation', async (req, res) => {
 });
 
 router.post('/test_validate_after_sync_relation_call', async (req, res) => {
-    await pepperi.addons.data.relations.upsert({
+    const cpiNodeService = new CPINodeService();
+    const relation = await cpiNodeService.upsertRelation({
         ...(CPI_NODE_VALIDATE_AFTER_SYNC_RELATION_CALL as Relation),
-        VALIDATE_AFTER_SYNC_CALL_FIELD_NAME,
+        VALIDATE_AFTER_SYNC_CALL_FIELD_NAME
     });
+    res.json({ relation });
 });
 
 router.post('/get_relations', async (req, res) => {
-    const relations = await pepperi.addons.data.relations.search(req.body);
+    const cpiNodeService = new CPINodeService();
+    const relations = await cpiNodeService.getRelations(req.body);
     res.json({ relations });
 });
 
 router.get('/root_dir', async (req, res) => {
-    const rootDir = await pepperi.files.rootDir();
+    const cpiNodeService = new CPINodeService();
+    const rootDir = await cpiNodeService.getRootDir();
     res.json({ rootDir });
 });
 
 router.get('/get_addons', async (req, res) => {
-    const allAddons = (
-        await pepperi.api.adal.getList({
-            addon: CPINodeAddonUUId,
-            table: ADDON_TABLE_NAME,
-        })
-    ).objects as AddonMetaData[];
-
+    const cpiNodeService = new CPINodeService();
+    const allAddons = await cpiNodeService.getAddons();
     res.json({ addons: allAddons });
 });
 
 router.get('/read_dir', async (req, res) => {
-    try {
-        const dirPath: string = req.query.dir_path as string;
-        const directories = await readdirAsync(dirPath);
-        res.json({ directories });
-    } catch (err) {
-        console.error('Error reading dir:', err);
-    }
-});
-
-router.post('/intercept_event', async (req, res) => {
-    const eventKey = req.body.eventKey;
-    await pepperi.events.intercept(eventKey, {}, async (eventData) => {
-        await pepperi.papiClient.addons.data
-            .uuid(CPINodeAddonUUId)
-            .table(CPI_NODE_TEST_TABLE)
-            .upsert(CPI_NODE_ADDON_VERSION_CHANGE_TEST_DATA_OBJECT);
-        console.log(`Intercepted event in intercept_event route: ${eventKey}`);
-    });
-    res.json({});
+    const cpinodeService = new CPINodeService();
+    const directories = await cpinodeService.readDir(req.query.dir_path as string);
+    res.json({ directories });
 });
 
 router.get('/cached_addons', async (req, res) => {
-    try {
-        const cachedAddons = require.cache;
-        res.json({ cachedAddonsNames: Object.keys(cachedAddons) });
-    } catch (err) {
-        console.error('Error reading cache:', err);
-    }
+    const cpinodeService = new CPINodeService();
+    const cachedAddons = cpinodeService.getCachedAddons();
+    res.json({ cachedAddonsNames: cachedAddons });
 });
--- cpi-side/addons-utils/cpi-node-utils/services/cpi-node-service.ts
+++ cpi-side/addons-utils/cpi-node-utils/services/cpi-node-service.ts
@@ -1,3 +1,36 @@
+import {ADDON_TABLE_NAME, AddonMetaData, CPINodeAddonUUId,} from '../consts';
+import * as fs from "fs";
 export class CPINodeService {
-    
+
+    async upsertRelation(relation: any) {
+        return await pepperi.addons.data.relations.upsert(relation);
+    }
+
+    async getRelations(params) {
+        return await pepperi.addons.data.relations.search(params);
+    }
+
+    async getRootDir() {
+        return await pepperi.files.rootDir();
+    }
+
+    async getAddons() {
+        const allAddons = (
+            await pepperi.api.adal.getList({
+                addon: CPINodeAddonUUId,
+                table: ADDON_TABLE_NAME,
+            })
+        ).objects as AddonMetaData[];
+        return allAddons;
+    }
+
+    async readDir(path: string) {
+        const directories = await fs.promises.readdir(path);
+        return directories;
+    }
+
+    getCachedAddons() {
+        const cachedAddons = require.cache;
+        return Object.keys(cachedAddons);
+    }
 }
